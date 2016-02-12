e579cfe45a5f0938e8a811c334f2b00f9ce9699c
Jackson Cafazzo <doctorjuno@gmail.com>
2016-02-11 22:49:51 -0800

    add new friend by id, go to new friend page and back to friend list page dispalying form to add new friend

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -12,6 +12,23 @@ namespace AddressBook
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/add_new_friend"] = _ => {
+        return View["friend-form.cshtml"];
+      };
+      Get["/friends_list"] = _ => {
+        var allContacts = Contact.GetAll();
+        return View["friend-list.cshtml", allContacts];
+      };
+      Post["/friend/new"] = _ => {
+        int phoneInt = int.Parse(Request.Form["friend-phone"]);
+        Contact newContact = new Contact(Request.Form["friend-name"], phoneInt, Request.Form["friend-address"]);
+        List<Contact> allContacts = Contact.GetAll();
+        return View["friend-list.cshtml", allContacts];
+      };
+      Get["/friend/{id}"] = parameters => {
+        var selectedContact = Contact.Find(parameters.id);
+        return View["friend.cshtml", selectedContact];
+      };
     }
   }
 }
