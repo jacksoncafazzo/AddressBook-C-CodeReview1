698851e414df23eb2ccd2e6c130da55bea3146aa
Jackson Cafazzo <doctorjuno@gmail.com>
2016-02-11 20:47:42 -0800

    add inital nancy and razor files plus contructor for Contact

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,17 @@
+using Nancy;
+using Contacts.Objects;
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
---
+++ Objects/Contact.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+
+namespace Contacts.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private int _phone;
+    private string _address;
+    private int _id;
+    private static List<Contact> _instances = new List<Contact> {};
+
+    public Contact(string name, int phone, string address)
+    {
+      _name = name;
+      _phone = phone;
+      _address = address;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public int GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(int newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Contact Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
